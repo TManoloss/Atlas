926753ee48a8613be771fc813a84c18885c87495
Manoel Felipe Carvalho <manoelfelipe90@gmail.com>
2024-02-08 22:35:21 -0300

    substituir chars pra bytes
    usar nivel de byte para processar texto ao inves de chats

--- nlu/model.py
+++ nlu/model.py
@@ -16,48 +16,33 @@ for command in data['commands']:
     
 #Processar texto: palavras, carcteres, bytes, sub-palavras
 
-chars = set()
 
-for input in inputs + outputs:
-    for ch in input:
-        if ch not in chars:
-            chars.add(ch)
-            
-#mapear char-index
 
-chr2idx = {}
-idx2chr = {}
+max_seq = max([len(bytes(x.encoding('utf-8'))) for x in inputs])
 
-# Iterar sobre os caracteres e seus índices
-for i, ch in enumerate(chars):
-    chr2idx[ch] = i
-    idx2chr[i] = ch
-
-max_seq = max([len(x) for x in inputs])
-
-print('Número de caracteres:', len(chars))
 print('Maior sequência:', max_seq)
 
 # Criar dataset one-hot (número de exemplos, tamanho da sequência, número de caracteres)
 # Criar dataset disperso (número de exemplos, tamanho da sequência)
 
 # input data one-hot encoding
-input_data = np.zeros((len(inputs), max_seq, len(chars)), dtype='int32')
 
-for i, input in enumerate(inputs):
-    for k, ch in enumerate(input):
-        input_data[i, k, chr2idx[ch]] = 1
+input_data = np.zeros((len(inputs), max_seq, 256), dtype='float32')
+
+for i, inp in enumerate(inputs):
+    for k, ch in enumerate(bytes(inp.encoding('utf-8'))):
+        input_data[i, k, int[ch]] = 1
 
 #Input data sparce
 
 
-        
+'''     
 input_data = np.zeros((len(inputs), max_seq), dtype='int32')
 
 for i, input in enumerate(inputs):
     for k, ch in enumerate(input):
         input_data[i, k] =chr2idx[ch] = 1
-        
+'''        
 #Output data
         
 labels = set(outputs)
@@ -79,8 +64,7 @@ output_data = to_categorical(output_data, len(output_data))
 print(output_data[0])
 
 model = Sequential()
-model.add(Embedding(max_seq, 64))
-model.add(LSTM(128, return_sequences=True, unroll=True))
+model.add(LSTM(128))
 model.add(Dense(len(output_data), activation='softmax'))
 
 model.compile(optimizer='adam',loss='categorical_crossentropy',  metrics=['acc'])
