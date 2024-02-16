a29675419ac21ca58324e0848e5f2e9dae664806
Manoel Felipe Carvalho <manoelfelipe90@gmail.com>
2024-02-15 21:32:40 -0300

    tests

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def evaluete(text):
     #Abrir programas
     elif entity == 'open|getnotePad':
         speak('Abrindo o bloco de notas...')
-        os.system('notepad')
+        os.system('start https://www.google.com')
     elif entity == 'open|getGoogle':
         speak('Abrindo o Gu gol...')
         os.system('start https://www.google.com')
