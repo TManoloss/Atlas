9abc9458fdfcb6e28ade87a19548ea220b8f3c31
Manoel Felipe Carvalho <manoelfelipe90@gmail.com>
2024-02-18 02:23:08 -0300

    tentando

--- nlu/classifier.py
+++ nlu/classifier.py
@@ -14,9 +14,8 @@ for k, label in enumerate(labels):
 
 # Classificar texto em um entidade
 def classify(text):
-    # Criar um array de entrada com caracteres ilimitados
-    max_chars = len(text)
-    x = np.zeros((1, max_chars, 256), dtype='float32')
+    # Criar um array de entrada
+    x = np.zeros((1, 21, 256), dtype='float32')
 
     # Preencher o array com dados do texto.
     for k, ch in enumerate(bytes(text.encode('utf-8'))):
@@ -26,7 +25,8 @@ def classify(text):
     out = model.predict(x)
     idx = out.argmax()
     return idx2label[idx]
-
+'''
 while True:
     text = input('Digite algo: ')
     print(classify(text))
+'''
